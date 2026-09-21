e0ecbaae67a7453d6b44b3e4dcfd26443cd94c2f
westernat <westernat233@163.com>
2026-09-21 21:12:37 +0800

    IdentityHashMap改成Reference2ObjectOpenHashMap 修复暴击率的问题

--- src/main/java/org/confluence/terra_curio/network/s2c/PlayerFlyPacketS2C.java
+++ src/main/java/org/confluence/terra_curio/network/s2c/PlayerFlyPacketS2C.java
@@ -1,5 +1,6 @@
 package org.confluence.terra_curio.network.s2c;
 
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
@@ -16,14 +17,14 @@ import org.confluence.terra_curio.client.handler.PlayerJumpHandler;
 import org.confluence.terra_curio.common.init.TCItems;
 import org.confluence.terra_curio.util.TCUtils;
 
-import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.function.IntFunction;
 
-public record PlayerFlyPacketS2C(Map<ResourceKey<Item>, MayFlyAbilityValue.FlyStack> flyStacks) implements IPacketS2C {
+public record PlayerFlyPacketS2C(
+        Map<ResourceKey<Item>, MayFlyAbilityValue.FlyStack> flyStacks) implements IPacketS2C {
     public static final Type<PlayerFlyPacketS2C> TYPE = new Type<>(TerraCurio.asResource("player_fly"));
     public static final StreamCodec<RegistryFriendlyByteBuf, PlayerFlyPacketS2C> STREAM_CODEC = ByteBufCodecs.map(
-            (IntFunction<Map<ResourceKey<Item>, MayFlyAbilityValue.FlyStack>>) IdentityHashMap::new,
+            (IntFunction<Map<ResourceKey<Item>, MayFlyAbilityValue.FlyStack>>) Reference2ObjectOpenHashMap::new,
             ResourceKey.streamCodec(Registries.ITEM), MayFlyAbilityValue.FlyStack.STREAM_CODEC
     ).map(PlayerFlyPacketS2C::new, PlayerFlyPacketS2C::flyStacks);
 
